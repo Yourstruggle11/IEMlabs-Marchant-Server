fe622a1775d9695ee4df5162dbb20b59009229ab
Yourstruggle11 <sensouvik51@gmail.com>
2022-06-10 22:31:07 +0530

    `fix`: Update `home route`

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -3,8 +3,15 @@ import  express  from "express";
 const route = express.Router();
 
 
-route.get("/", function(req, res){
-    res.send("hello")
+const time = new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' })
+
+route.get('/', function (req, res) {
+    res.json({
+        message: 'IEMlabs Merchant Main server',
+        Note: 'This is an interview assignment, This has nothing to do with iemlabs, I am not bracking any copyright law',
+        deployTime: time,
+      })
 })
+
 
 export default route;
